545979a2f8239755d6c2183cfcdc0f616645cd1e
Grazielly Araujo <grazielly.araujo@outlook.com>
2022-08-18 13:57:19 -0300

    Projeto Blog Pessoal

---
+++ src/main/java/com/generation/blogpessoal/configuration/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.generation.blogpessoal.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class SwaggerConfig {
+	
+	@Bean
+	public OpenAPI springBlogPessoalOpenAPI() {
+		return new OpenAPI()
+				.info(new Info()
+					.title("Projeto Blog Pessoal")
+					.description("Projeto Blog Pessoal - Generation Brasil")
+					.version("v0.0.1")
+				.license(new License()
+					.name("Generation Brasil")
+					.url("https://brazil.generation.org/"))
+				.contact(new Contact()
+					.name("Conteudo Generation")
+					.url("https://github.com/conteudoGeneration")
+					.email("conteudogeneration@gmail.com")))
+				.externalDocs(new ExternalDocumentation()
+					.description("Github")
+					.url("https://github.com/conteudoGeneration/"));
+	}
+
+}
